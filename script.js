35984e10e593c802c45b04e3d92008c6ee39679a
Linux Adona <linuxadona17@gmail.com>
2025-01-07 16:55:32 +0800

    Added Animation

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 window.addEventListener("load", () => {
-    document.body.style.overflow = "visible"; // Enable scrolling after animations
-});
+    document.body.style.overflow = "visible";
+    const aboutSection = document.querySelector('.about');
 
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                aboutSection.classList.add('visible'); // Add the visible class
+                observer.unobserve(aboutSection); // Stop observing after it becomes visible
+            }
+        });
+    });
+
+    observer.observe(aboutSection);
+});
