48d7f8e2125a7d020b19d0c924df858d8ba7bc56
Linux Adona <linuxadona17@gmail.com>
2025-09-26 03:07:06 +0800

    Responsiveness improvised

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
 // Redirect to login.html after a short delay
 setTimeout(function() {
     window.location.href = "login.html";
-}, 5000); // 5000 milliseconds delay
+}, 1000); // 5000 milliseconds delay
